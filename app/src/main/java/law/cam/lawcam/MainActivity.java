d5dc41be321aa9d32399f62832e896607ec7c5e1
DongKyu Kim <aass0903@naver.com>
2021-01-08 19:06:28 +0900

    initial commit

---
+++ app/src/main/java/law/cam/lawcam/MainActivity.java
@@ -0,0 +1,14 @@
+package law.cam.lawcam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
