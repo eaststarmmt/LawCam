61cefa01d50c57e8107550f595f9347d73e1c57e
DongKyu Kim <aass0903@naver.com>
2021-01-09 22:16:42 +0900

    2021-01-09 It is not over yet

--- app/src/main/java/law/cam/lawcam/MainActivity.java
+++ app/src/main/java/law/cam/lawcam/MainActivity.java
@@ -1,14 +1,73 @@
 package law.cam.lawcam;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    File file;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 101 && resultCode== RESULT_OK) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+
+            imageView.setImageBitmap(bitmap);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        ImageButton camButton = findViewById(R.id.camButton);
+        camButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
+    }
+
+    public void takePicture() {
+        if(file == null) {
+            file = createFile();
+        }
+        Uri fileUri = FileProvider.getUriForFile(this, "law.cam.lawcam.intent.fileprovider", file);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 101);
+        }
+    }
+
+    private File createFile() {
+        String filename = "capture.jpg";
+        File storageDir = Environment.getExternalStorageDirectory();
+        File outFile = new File(storageDir, filename);
+
+        return outFile;
     }
 }
